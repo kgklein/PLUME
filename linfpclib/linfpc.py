Big update with several fixes: 1st. fixed calc of deriv of 0th bessel function (can't use identity when n = 0). 2nd. Fixed sign error between swanson and stix (above equation 4.180 in swanson, the authors claim to supress exp(i(k dot r - om t)) to get fs1 in freq space, but formally they must take the fourier transform, which leads to terms like delta(kx0+kx) and delta(om0-om), (note that when swanson takes the fourier transform of a function with kx,ky,kz or om in it, say F{exp(i om t)} = f(om), om on the lhs is techincally/subtly a different variable than om on the right hand side, despite the reuse of the symbol), these delta terms should result in replacing kx <- -kx etc... Normally, this does not matter, as it is effectively a change of coordinate system. We can still get the same disp rels with this mistake. However, for the case of correlating fs1 from swanson and the eigenfuncs from stix, we get an unwanted minus sign. This is accounted for in the final computation of the correlation by dropping the minus sign in corei = -q v^2 dfdv ei. 3rd. Fixed orientation issue with cartesian plot. Results were carefully checked with debug statements that directly printed correlation values and coordinates. Also added variables to the dictionary that loads cart fpc results (ex: vx_xy vy_xy ...) that are parallel to the fpc data in the same dictionary. The plot routine plot_9pan_cart is now correct and well tested. Results for MMS ICD test case now agree with results from Aryas et al recently re submitted Nat comm paper. NOTE: this push contains many debug statements that will be removed soon in an upcoming push that will clean up the code

linfpclib/linfpc.py
@@ -749,7 +749,7 @@ def loadlinfpccepar(filename):
     # C = np.flip(C,axis=1) #TODO: perhaps this should be done in the plotting routine #TODO: remove?
     C = np.asarray(C)
 
-        #weird index rounding bug fix
+    #weird index rounding bug fix
     if(len(vpar) < len(C[0])):
         vpar.append(float(vparindex))
     elif(len(vpar) > len(C[0])):
@@ -804,10 +804,25 @@ def loadlinfpccart(filename):
 
     resonant_int = resonant_int.real
 
+    ivxmin=int(vxmin/delv)
+    ivxmax=int(vxmax/delv)
+    ivymin=int(vymin/delv)
+    ivymax=int(vymax/delv)
+    ivzmin=int(vzmin/delv)
+    ivzmax=int(vzmax/delv)
 
-    numstepvx = int(math.floor((vxmax-vxmin)/delv)) #TODO: implement this fix into all load functions
-    numstepvy = int(math.floor((vymax-vymin)/delv))
-    numstepvz = int(math.floor((vzmax-vzmin)/delv))
+    numstepvx = ivxmax-ivxmin+1
+    numstepvy = ivymax-ivymin+1
+    numstepvz = ivzmax-ivzmin+1
+
+    vx_xy = np.zeros((numstepvx,numstepvy))
+    vy_xy = np.zeros((numstepvx,numstepvy))
+
+    vx_xz = np.zeros((numstepvx,numstepvz))
+    vz_xz = np.zeros((numstepvx,numstepvz))
+
+    vy_yz = np.zeros((numstepvy,numstepvz))
+    vz_yz = np.zeros((numstepvy,numstepvz))
 
     vx = []
     vy = []
@@ -841,14 +856,18 @@ def loadlinfpccart(filename):
     Cvxvy = []
     line = f.readline()
     linecounter = 1
-    while (linecounter <= len(vx)):
+    while (linecounter < len(vx)):
         line = line.split()
         row = []
+        rowcounter = 0
         for k in line:
             if(math.isnan(float(k))):
                 row.append(0.)
             else:
                 row.append(float(k))
+            vx_xy[linecounter-1,rowcounter] = vx[linecounter-1]
+            vy_xy[linecounter-1,rowcounter] = vy[rowcounter]
+            rowcounter += 1
         Cvxvy.append(row)
         line = f.readline()
         linecounter += 1
@@ -857,14 +876,18 @@ def loadlinfpccart(filename):
     Cvxvz = []
     linecounter = 1
     line = f.readline()
-    while (linecounter <= len(vx)):
+    while (linecounter < len(vx)):
         line = line.split()
         row = []
+        rowcounter = 0
         for k in line:
             if(math.isnan(float(k))):
                 row.append(0.)
             else:
                 row.append(float(k))
+            vx_xz[linecounter-1,rowcounter] = vx[linecounter-1]
+            vz_xz[linecounter-1,rowcounter] = vz[rowcounter]
+            rowcounter += 1
         Cvxvz.append(row)
         line = f.readline()
         linecounter += 1
@@ -873,18 +896,24 @@ def loadlinfpccart(filename):
     Cvyvz = []
     linecounter = 1
     line = f.readline()
-    while (linecounter <= len(vy)):
+    while (linecounter < len(vy)):
         line = line.split()
         row = []
+        rowcounter = 0
         for k in line:
             if(math.isnan(float(k))):
                 row.append(0.)
             else:
                 row.append(float(k))
+            vy_yz[linecounter-1,rowcounter] = vy[linecounter-1]
+            vz_yz[linecounter-1,rowcounter] = vz[rowcounter]
+            rowcounter += 1
         Cvyvz.append(row)
         line = f.readline()
         linecounter += 1
     Cvyvz = np.asarray(Cvyvz)
+    vy_yz = vy_yz.T
+    vz_yz = vz_yz.T
 
     linfpcckeyname = 'CEpar'
     if('perp1' in filename):
@@ -892,7 +921,10 @@ def loadlinfpccart(filename):
     elif('perp2' in filename):
         linfpcckeyname = 'CEperp2'
 
-    linfpcdata = {linfpcckeyname+'vxvy':Cvxvy,linfpcckeyname+'vxvz':Cvxvz,linfpcckeyname+'vyvz':Cvyvz,'vx':vx,'vy':vy,'vz':vz,'resonant_int':resonant_int,'vxmin':vxmin,'vxmax':vxmax,'vymin':vymin,'vymax':vymax,'vzmin':vzmin,'vzmax':vzmax,'delv':delv,'species':species,'omega':omega}
+    linfpcdata = {linfpcckeyname+'vxvy':Cvxvy,linfpcckeyname+'vxvz':Cvxvz,linfpcckeyname+'vyvz':Cvyvz,
+                  'vx':vx,'vy':vy,'vz':vz,'resonant_int':resonant_int,'vxmin':vxmin,'vxmax':vxmax,'vymin':vymin,'vymax':vymax,'vzmin':vzmin,'vzmax':vzmax,
+                  'delv':delv,'species':species,'omega':omega,
+                  'vx_xy':vx_xy,'vy_xy':vy_xy,'vx_xz':vx_xz,'vz_xz':vz_xz,'vy_yz':vy_yz,'vz_yz':vz_yz}
 
     return linfpcdata
 
@@ -1169,6 +1201,68 @@ def load_plume_sweep(flnm,verbose=True,use_ps_split_new=False): #TODO: check whi
         plume_sweep['bzi'] = plume_sweep['bzi']*plume_sweep['vtp']
 
         return plume_sweep
+
+def loadeigen(flnm):
+
+    eigendict = {}
+
+    f = open(flnm)
+    line = f.readline()
+    line = line.split()
+
+    #TODO: this assumes nspec = 2. TODO: generalize for any num of nspec
+
+    eigendict['kperp'] = line[0]
+    eigendict['kpar'] = line[1]
+    eigendict['betap'] = line[2]
+    eigendict['vtp'] = line[3]
+    eigendict['omega'] = complex(float(line[4]),float(line[5]))
+    eigendict['bx'] = complex(float(line[6]),float(line[7]))
+    eigendict['by'] = complex(float(line[8]),float(line[9]))
+    eigendict['bz'] = complex(float(line[10]),float(line[11]))
+    eigendict['ex'] = complex(float(line[12]),float(line[13]))
+    eigendict['ey'] = complex(float(line[14]),float(line[15]))
+    eigendict['ez'] = complex(float(line[16]),float(line[17]))
+    eigendict['uxi'] = complex(float(line[18]),float(line[19]))
+    eigendict['uyi'] = complex(float(line[20]),float(line[21]))
+    eigendict['uzi'] = complex(float(line[22]),float(line[23]))
+    eigendict['uxe'] = complex(float(line[24]),float(line[25]))
+    eigendict['uye'] = complex(float(line[26]),float(line[27]))
+    eigendict['uze'] = complex(float(line[28]),float(line[29]))
+    eigendict['ni'] = complex(float(line[30]),float(line[31]))
+    eigendict['ne'] = complex(float(line[32]),float(line[33]))
+    eigendict['Pi'] = complex(float(line[34]),float(line[35]))
+    eigendict['Pe'] = complex(float(line[36]),float(line[37]))
+
+    #TODO: load Ps_split_new
+
+    return eigendict
+
+def loadmoms(flnm):
+
+    momsdict = {}
+
+    f = open(flnm)
+    line = f.readline()
+    line = line.split()
+
+    momsdict['ni'] = complex(float(line[0]),float(line[1]))
+    momsdict['ne'] = complex(float(line[2]),float(line[3]))
+    momsdict['uxi'] = complex(float(line[4]),float(line[5]))
+    momsdict['uyi'] = complex(float(line[6]),float(line[7]))
+    momsdict['uzi'] = complex(float(line[8]),float(line[9]))
+    momsdict['uxe'] = complex(float(line[10]),float(line[11]))
+    momsdict['uye'] = complex(float(line[12]),float(line[13]))
+    momsdict['uze'] = complex(float(line[14]),float(line[15]))
+    momsdict['jxiex'] = float(line[16])
+    momsdict['jyiey'] = float(line[17])
+    momsdict['jziez'] = float(line[18])
+    momsdict['jxeex'] = float(line[19])
+    momsdict['jyeey'] = float(line[20])
+    momsdict['jzeez'] = float(line[21])
+
+    return momsdict
+
     
 def double_k_scan_from_root(plume_input,ktotsweepmin,ktotsweepmax,root,inputflnm,outputname,outlog='outlog',nsamps=200):
     #makes sweep over kperp and kpar from k0=np.sqrt(kperp^2+kpar^2) to k1=np.sqrt(kperp^2+kpar^2)

linfpclib/linfpcplot.py
@@ -212,7 +212,13 @@ def plot_9pan_cart(foldername,flnm='',specnum='01',computeEner=False, scaleveloc
         for cartcdata in cartcdatas:
             ckey = ckeyprefixes[_j]+dirkey
             absmax = np.max(np.abs(cartcdata[ckey]))
-            _tempim = axs[_j,_i].pcolormesh(np.asarray(cartcdata[dirkey[2:4]])*scalevelocity,np.asarray(cartcdata[dirkey[0:2]])*scalevelocity,np.asarray(cartcdata[ckey]).T[:,:],vmax=absmax,vmin=-absmax,cmap="seismic")
+            # if(dirkey == 'vyvz'):
+            #     _tempim = axs[_j,_i].pcolormesh(np.asarray(cartcdata[dirkey[2:4]])*scalevelocity,np.asarray(cartcdata[dirkey[0:2]])*scalevelocity,np.asarray(cartcdata[ckey])[:,:],vmax=absmax,vmin=-absmax,cmap="seismic")
+            # else:
+            #     _tempim = axs[_j,_i].pcolormesh(np.asarray(cartcdata[dirkey[2:4]])*scalevelocity,np.asarray(cartcdata[dirkey[0:2]])*scalevelocity,np.asarray(cartcdata[ckey]).T[:,:],vmax=absmax,vmin=-absmax,cmap="seismic")
+            _plot1 = cartcdata['v'+dirkey[1]+'_'+dirkey[1]+dirkey[3]]
+            _plot2 = cartcdata['v'+dirkey[3]+'_'+dirkey[1]+dirkey[3]]
+            _tempim = axs[_j,_i].pcolormesh(np.asarray(_plot1)*scalevelocity,np.asarray(_plot2)*scalevelocity,np.asarray(cartcdata[ckey])[:,:],vmax=absmax,vmin=-absmax,cmap="seismic")
             axs[_j,_i].grid()
             axs[_j,_i].set_title(titlesprefixes[_j]+titlesuffixes[_i])
             axs[_j,_i].set_xlabel(xaxlabels[_i])